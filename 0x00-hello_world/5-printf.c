bf6d9efff2e5fd81e22f3a0c3ac1f1ced68ef454
Kehinde <kensawyerr@gmail.com>
2023-12-21 18:34:08 -0800

    5-printf.c is working now

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - using the printf command, print a line of code
+ * main - Entry point
  *
- * Return : Always 0 (Success)
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
